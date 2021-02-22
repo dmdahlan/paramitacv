08c693c076592f2158a6319b874f8c49ec07353b
dmdahlan <muh_dahlan86@yahoo.co.id>
2021-02-22 14:25:51 +0800

    bug biaya

--- app/Views/data/vw_delivbiaya.php
+++ app/Views/data/vw_delivbiaya.php
@@ -403,7 +403,7 @@
                 [20, 100, 500, 1500],
                 [20, 100, 500, 1500]
             ],
-            "scrollY": 620,
+            "scrollY": 720,
             "scrollX": true,
             "scrollCollapse": true,
             "fixedColumns": {
